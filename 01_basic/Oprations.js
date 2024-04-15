6689847f65bb9934f5b8671a993d9707ccfa1fd0
yashshinde8585 <162013813+yashshinde8585@users.noreply.github.com>
2024-04-15 10:10:06 +0000

    Oprations

---
+++ 01_basic/Oprations.js
@@ -0,0 +1,27 @@
+let value = 3 
+let negValue = -value 
+// console.log(negValue);
+
+/*
+let str1 = "hello"
+let str2 = " yash"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+*/
+
+console.log(+true); //1 
+console.log(+""); //0
+
+let num1, num2, num3 
+ 
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100 
+// gameCounter++;
+++gameCounter; 
+console.log(gameCounter);
