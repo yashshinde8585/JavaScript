1274f57a0fcf74fe761ca3f390d04d52fbdbeb58
yashshinde8585 <162013813+yashshinde8585@users.noreply.github.com>
2024-04-17 12:20:36 +0000

    Math and num

---
+++ 01_basic/Maths.js
@@ -0,0 +1,18 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(3.9));
+// console.log(Math.sqrt(3));
+// console.log(Math.floor(3));
+// console.log(Math.min(6,1,4,5,3));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+
