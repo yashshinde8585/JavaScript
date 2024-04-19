c68dadb1b0f7053d3f1364993368bec8e9e07331
yashshinde8585 <162013813+yashshinde8585@users.noreply.github.com>
2024-04-19 13:11:08 +0000

    String Methods

---
+++ 01_basic/stringMethods.js
@@ -0,0 +1,28 @@
+// let msg = "   hello i am yash   ";
+// console.log(msg);
+// let str = msg.trim();
+// console.log(str);
+
+// let newMes = msg.trim().toUpperCase(); //Method chaining (excuting left to right)
+// console.log(newMes);
+
+// let indexof = msg.indexOf("i"); //9
+// let indexof = msg.indexOf("I"); //-1 (-1 means not found)
+// let indexof = msg.indexOf("a"); //11 
+// console.log(indexof); 
+
+// let str = "hello i am yash";
+// console.log(str);
+/*
+let newRep1 = str.replace("hello","HELLO");
+let newRep2 = str.replace("y","Y");
+let newRep3 = str.replace(" ","-");
+console.log(newRep1); //HELLO i am yash
+console.log(newRep2); //hello i am Yash
+console.log(newRep3); //hello-i am yash
+*/
+
+let str = "Hello  ";
+console.log(str);
+let rep = str.repeat(3)
+console.log(rep);
